1eb7be057b3654f44607c620cd8e5176a2097e30
Jens Klingenberg <jens.klingenberg@edeka.de>
2021-03-07 19:12:39 +0100

    cleanup

---
+++ iosApp/iosApp/MealViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  ContributorsResource.swift
+//  iosApp
+//
+//  Created by Jens Klingenberg on 04.03.21.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+import shared
+
+final class MealViewModel: ObservableObject {
+   
+    @Published var drinks = [Meal]()
+    @Published var categories = [shared.Category]()
+
+
+    private let repository: MealDataSource
+
+       init(repository: MealDataSource) {
+           self.repository = repository
+       }
+       
+       func startObservingPeopleUpdates() {
+            repository.getMeals { cocktailResult,_ in
+                self.drinks = cocktailResult ?? [Meal]()
+            }
+       }
+    
+    func loadCategories() {
+         repository.getCategories{ categories,_ in
+             self.categories = categories!
+         }
+    }
+    
+    func searchMealsByName(name:String) {
+        repository.getMealsByName(categoryName: name){ categories,_ in
+            self.drinks = categories  ??  [Meal]()
+         }
+    }
+    
+        func getIngredientImageUrl(personName: String) -> String {
+            return repository.getIngredientImageUrl(name: personName)
+        }
+       
+}
